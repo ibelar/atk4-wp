6c0059eb1b079052ef996699ec07e4c24ec7d845
Alain Belair <belair.alain@gmail.com>
2016-05-06 14:37:37 -0400

    initial dashboard commit
    Add ability to include dashboard widget managed by atk4 in WP.

---
+++ lib/Wp/WpDashboard.php
@@ -0,0 +1,17 @@
+<?php
+
+/* =====================================================================
+ * Atk4-wp => An Agile Toolkit PHP framework interface for WordPress.
+ *
+ * This interface enable the use of the Agile Toolkit framework within a WordPress site.
+ *
+ * Please note that atk or atk4 mentioned in comments refer to Agile Toolkit or Agile Toolkit version 4.
+ * More information on Agile Toolkit: http://www.agiletoolkit.org
+ *
+ * Author: Alain Belair
+ * Licensed under MIT
+ * =====================================================================*/
+/**
+ * Wp Dashboard Widget
+ */
+class Wp_WpDashboard extends Wp_WpPanel {}
